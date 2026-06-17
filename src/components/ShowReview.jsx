bfceddace6e58f8a1bc4092dc8ac05f73e4336f6
Sau_Rav <samirpangeni37@gmail.com>
2026-06-17 21:10:02 +0545

    change fix the error

--- src/components/ShowReview.jsx
+++ src/components/ShowReview.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import Loading from "./Loading";
 
 const ShowReview = ({ productId }) => {
