7fd9d029d5ccad65cbc6a76ccc10e5a20254ba5a
Sau_Rav <samirpangeni37@gmail.com>
2026-06-17 20:57:47 +0545

    change page.js

--- src/app/chekout/[id]/page.js
+++ src/app/chekout/[id]/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+import { useRouter } from "next/navigation";
 const Page = ({ params }) => {
   const orderId = params.id;
   const payWithKhalti = async () => {
