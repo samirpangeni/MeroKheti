bb24d318a6c94189da2ae26ca3f6b375315d3611
Sau_Rav <samirpangeni37@gmail.com>
2026-06-19 14:39:47 +0545

    change in head

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="wF5KH93xlnahWurLhdeI3ztZD0C5Am0fO-cRq-jhZRE"
+      />
+    </>
+  );
+}
