48206e4a4d0a81a378a900390f1c66e02adc2b32
Sau_Rav <samirpangeni37@gmail.com>
2026-06-19 14:18:17 +0545

    google console

--- src/app/head.js
+++ src/app/head.js
@@ -1,10 +1,9 @@
 export default function Head() {
-  return (
-    <>
-      <meta
-        name="google-site-verification"
-        content="wF5KH93xlnahWurLhdeI3ztZD0C5Am0fO-cRq-jhZRE"
-      />
-    </>
-  );
+    return (
+        <>
+            <meta
+                name="google-site-verification" content="wF5KH93xlnahWurLhdeI3ztZD0C5Am0fO-cRq-jhZRE"
+            />
+        </>
+    );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,11 @@ import "./globals.css";
 import "../../lib/cron"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Head from "next/head";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <meta name="google-site-verification" content="wF5KH93xlnahWurLhdeI3ztZD0C5Am0fO-cRq-jhZRE" />
-      </Head>
+     
       <body className="min-h-screen bg-white text-black antialiased">
         {children}
           <ToastContainer position="top-right" />
