715ce0a0d04738f1ef044afebbf2c0f8a3c15de4
Sau_Rav <samirpangeni37@gmail.com>
2026-06-19 14:52:17 +0545

    meat check

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,9 @@ import "react-toastify/dist/ReactToastify.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-
+      <head>
+        <meta name="test-meta" content="hello123" />
+      </head>
       <body className="min-h-screen bg-white text-black antialiased">
         {children}
         <ToastContainer position="top-right" />
