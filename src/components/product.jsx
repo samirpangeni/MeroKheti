3555a58d4a975f6877b040ea7db0f951e3784d5f
Sau_Rav <samirpangeni37@gmail.com>
2026-08-18 18:07:48 +0545

    imporve UI

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const product = () => {
   return (
     <div className=' bg-green-600 text-black p-3 rounded-full h-15 w-15 sm:rounded-full'>
-      <Link href="/addProduct" className='flex gap-2 items-center justify-center'><p className="text-4xl">+</p> <p className='hidden md:block'>Upload product</p></Link>
+      <Link href="/addProduct" className='flex gap-2 items-center justify-center'><p className="text-4xl">+</p></Link>
     </div>
   )
 }
